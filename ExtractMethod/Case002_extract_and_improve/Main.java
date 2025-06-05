d7a3b25c2a2050d7384e050ebf741c1b8933b618
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 05:39:11 +0900

    feat: add Extract Method (Case002_extract_and_improve) before refactoring

---
+++ ExtractMethod/Case002_extract_and_improve/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 0;
+        if (y != 0) {
+            int result = x / y;
+            System.out.println("Quotient: " + result);
+        } else {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
