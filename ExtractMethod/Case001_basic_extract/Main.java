38c001759950e7da0208831d2a64c32863db66dc
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 05:37:59 +0900

    feat: add Extract Method (Case001_basic_extract) before refactoring

---
+++ ExtractMethod/Case001_basic_extract/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int sum = x + y;
+        System.out.println("Sum: " + sum);
+    }
+}
