e2317f715705b6dce0640a6607a499b1dc4009db
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 05:38:29 +0900

    refactor: apply Extract Method (Case001_basic_extract) refactoring

--- ExtractMethod/Case001_basic_extract/Main.java
+++ ExtractMethod/Case001_basic_extract/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int x = 5;
         int y = 3;
+        printSum(x, y);
+    }
+
+    public static void printSum(int x, int y) {
         int sum = x + y;
         System.out.println("Sum: " + sum);
     }
