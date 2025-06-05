306adf66f0de65de64dbfa32dbee9c80906e633a
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 05:51:05 +0900

    feat: add Extract Method (Case002_extract_and_improve) before refactoring

---
+++ ExtractMethod/Case002_extract_and_improve/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        if (b == 0) {
+            System.out.println("Error: Division by zero");
+        } else {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        }
+    }
+}
