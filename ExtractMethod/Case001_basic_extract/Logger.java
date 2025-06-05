15145e62e09477749e571a8e891cb81289fc63da
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 05:20:53 +0900

    refactor: apply Extract Method (Case001_basic_extract) refactoring

--- ExtractMethod/Case001_basic_extract/Logger.java
+++ ExtractMethod/Case001_basic_extract/Logger.java
@@ -1,6 +1,10 @@
 public class Logger {
     public void logInfo(String msg) {
+        printHeader();
         System.out.println("INFO: " + msg);
+    }
+
+    private void printHeader() {
         System.out.println("------");
     }
 }
